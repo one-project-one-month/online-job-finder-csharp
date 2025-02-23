96e9100d32d34651b2115a09ebf767f425afbc1b
Paing Soe Thu <95268659+paingsoethu-coding@users.noreply.github.com>
2025-02-23 23:47:52 +0700

    feat: add new db

---
+++ online-job-finder/online-job-finder.DataBase/Models/TblJobCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace online_job_finder.DataBase.Models;
+
+public partial class TblJobCategory
+{
+    public Guid JobCategoriesId { get; set; }
+
+    public string Industry { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public int Version { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public virtual ICollection<TblApplicantJobCategory> TblApplicantJobCategories { get; set; } = new List<TblApplicantJobCategory>();
+
+    public virtual ICollection<TblJob> TblJobs { get; set; } = new List<TblJob>();
+}
---
+++ online-job-finder/online-job-finder.DataBase/Models/TblUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace online_job_finder.DataBase.Models;
+
+public partial class TblUser
+{
+    public Guid UserId { get; set; }
+
+    public Guid RoleId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string ProfilePhoto { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool IsInformationCompleted { get; set; }
+
+    public int Version { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public virtual TblRole Role { get; set; } = null!;
+
+    public virtual ICollection<TblApplicantProfile> TblApplicantProfiles { get; set; } = new List<TblApplicantProfile>();
+
+    public virtual ICollection<TblCompanyProfile> TblCompanyProfiles { get; set; } = new List<TblCompanyProfile>();
+
+    public virtual ICollection<TblResume> TblResumes { get; set; } = new List<TblResume>();
+
+    public virtual ICollection<TblSocialMedium> TblSocialMedia { get; set; } = new List<TblSocialMedium>();
+}
