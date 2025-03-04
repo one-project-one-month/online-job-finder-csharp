726606d94eb38b288a586775b6971ccbdc908946
Eulean <eulen9t5@gmail.com>
2025-03-04 22:12:22 +0630

    feat: implement company profile services and job management models

---
+++ online-job-finder/online-job-finder.Domain/Services/CompanyProfileServices/ICompanyProfileServices.cs
@@ -0,0 +1,16 @@
+using System;
+using online_job_finder.Domain.Services.CompanyProfileServices.CompanyProfileResponses;
+
+namespace online_job_finder.Domain.Services.CompanyProfileServices;
+
+public interface ICompanyProfileServices
+{
+    List<CompanyProfileJobViewModel> GetCompanyJobs(Guid companyId);
+    CompanyProfileJobViewModel CreateJob(CompanyProfileJobViewModel job);
+    CompanyProfileJobViewModel? UpdateJob(string jobId, CompanyProfileJobViewModel job);
+    bool DeleteJob(string jobId);
+    List<ApplicationViewModel> GetJobApplications(string jobId);
+    List<ApplicationViewModel> GetShortlistedApplications(string jobId);
+    bool UpdateApplicationStatus(string applicationId, string status);
+    List<ReviewViewModel> GetCompanyReviews(Guid companyId);
+}
